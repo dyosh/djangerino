9c6d4109d37388e468ef49b0ace0c69a3f009adc
Dan <dantakayosh@gmail.com>
2016-02-25 04:40:43 -0500

    added comment model

--- blogerino/blog/urls.py
+++ blogerino/blog/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<post_id>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<post_id>\d+)/delete/$', views.post_delete, name='post_delete'),
+    url(r'^post/(?P<post_id>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+
+    url(r'^comment/(?P<comment_id>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+    url(r'^comment/(?P<comment_id>\d+)/approve/$', views.comment_approve, name='comment_approve'),
 ]
